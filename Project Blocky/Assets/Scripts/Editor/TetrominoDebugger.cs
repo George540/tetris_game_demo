817524eb75f28c73df257dbe39801fe8c498bdd9
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-06-06 20:48:49 -0400

    Add Manual Tetromino Waitlist on Debugger

--- Project Blocky/Assets/Scripts/Editor/TetrominoDebugger.cs
+++ Project Blocky/Assets/Scripts/Editor/TetrominoDebugger.cs
@@ -1,24 +1,68 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
-
 
 public class TetrominoDebugger : EditorWindow
 {
-    [MenuItem("Window/UI Toolkit/TetrominoDebugger")]
-    public static void ShowExample()
+    private WaitlistBoard _waitlistBoard;
+    
+    private Button _tetrominoI;
+    private Button _tetrominoJ;
+    private Button _tetrominoL;
+    private Button _tetrominoO;
+    private Button _tetrominoS;
+    private Button _tetrominoT;
+    private Button _tetrominoZ;
+
+    private Button _eraseQueueButton;
+    
+    
+    [MenuItem("Debug/Tetromino Debugger _%#D")]
+    public static void ShowWindow()
     {
-        TetrominoDebugger wnd = GetWindow<TetrominoDebugger>();
-        wnd.titleContent = new GUIContent("TetrominoDebugger");
+        var window = GetWindow<TetrominoDebugger>();
+        window.titleContent = new GUIContent("TetrominoDebugger");
+        window.minSize = new Vector2(350, 350);
+        window.Show();
     }
 
-    public void CreateGUI()
+    public void OnEnable()
     {
+        InitializeUxmlTemplate();
+        
+        _waitlistBoard = FindObjectOfType<WaitlistBoard>();
+        
         // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
+        var root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        var buttonRoot = rootVisualElement.Q("TetrominoButtonsRoot");
+
+        _tetrominoI = buttonRoot.Q<Button>("ButtonI");
+        _tetrominoI.clicked += OnButtonIPressed;
+        
+        _tetrominoJ = buttonRoot.Q<Button>("ButtonJ");
+        _tetrominoJ.clicked += OnButtonJPressed;
+        
+        _tetrominoL = buttonRoot.Q<Button>("ButtonL");
+        _tetrominoL.clicked += OnButtonLPressed;
+        
+        _tetrominoO = buttonRoot.Q<Button>("ButtonO");
+        _tetrominoO.clicked += OnButtonOPressed;
+        
+        _tetrominoS = buttonRoot.Q<Button>("ButtonS");
+        _tetrominoS.clicked += OnButtonSPressed;
+        
+        _tetrominoT = buttonRoot.Q<Button>("ButtonT");
+        _tetrominoT.clicked += OnButtonTPressed;
+        
+        _tetrominoZ = buttonRoot.Q<Button>("ButtonZ");
+        _tetrominoZ.clicked += OnButtonZPressed;
+
+        _eraseQueueButton = buttonRoot.Q<Button>("EraseQueue");
+        _eraseQueueButton.clicked += OnEraseQueue;
+
+
+        /*// VisualElements objects can contain other VisualElement following a tree hierarchy.
         VisualElement label = new Label("Hello World! From C#");
         root.Add(label);
 
@@ -32,6 +76,53 @@ public class TetrominoDebugger : EditorWindow
         var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/Editor/TetrominoDebugger.uss");
         VisualElement labelWithStyle = new Label("Hello World! With Style");
         labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
+        root.Add(labelWithStyle);*/
+    }
+    
+    private void InitializeUxmlTemplate()
+    {
+        rootVisualElement.Clear();
+        var template = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/TetrominoDebugger.uxml");
+        template.CloneTree(rootVisualElement);
+    }
+
+    private void OnButtonIPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(0));
+    }
+    
+    private void OnButtonJPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(1));
+    }
+    
+    private void OnButtonLPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(2));
+    }
+    
+    private void OnButtonOPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(3));
+    }
+    
+    private void OnButtonSPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(4));
+    }
+    
+    private void OnButtonTPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(5));
+    }
+    
+    private void OnButtonZPressed()
+    {
+        _waitlistBoard.AddTetrominoOnWaitingList(_waitlistBoard.GetTetrominoData(6));
+    }
+
+    private void OnEraseQueue()
+    {
+        _waitlistBoard.EraseManualWaitlist();
     }
 }
--- Project Blocky/Assets/Scripts/WaitlistBoard.cs
+++ Project Blocky/Assets/Scripts/WaitlistBoard.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class WaitlistBoard : MonoBehaviour
 {
     [SerializeField] private GameManager _gameManager;
     [SerializeField] private TetrominoData[] _tetrominoesData;
     [SerializeField] private TetrominoDisplay[] _tetrominoDisplays = new TetrominoDisplay[2];
-    private Queue<TetrominoData> _waitlistTetrominoData = new Queue<TetrominoData>();
+    [SerializeField] private readonly Queue<TetrominoData> _waitlistTetrominoData = new();
 
     private void Awake()
     {
@@ -21,5 +18,20 @@ public class WaitlistBoard : MonoBehaviour
                 display.Initialize(_gameManager, _tetrominoesData[choice]);
             }
         }
+    }
+
+    public TetrominoData GetTetrominoData(int choice)
+    {
+        return _tetrominoesData[choice];
+    }
+    
+    public void AddTetrominoOnWaitingList(TetrominoData data)
+    {
+        _waitlistTetrominoData.Enqueue(data);
+    }
+
+    public void EraseManualWaitlist()
+    {
+        _waitlistTetrominoData.Clear();
     }
 }
